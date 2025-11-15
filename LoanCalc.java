5a143acf5e984e9c1ff4998ebc5d8bca65d46afb
gilsalem <gil.salem@post.runi.ac.il>
2025-11-15 22:09:20 +0200

    finish

--- LoanCalc.java
+++ LoanCalc.java
@@ -9,55 +9,56 @@ public class LoanCalc {
         int n = Integer.parseInt(args[2]);
         System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
+        // Computes the periodical payment using brute force
         System.out.print("\nPeriodical payment, using brute force: ");
         System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
         System.out.println("number of iterations: " + iterationCounter);
 
+        // Computes the periodical payment using bisection search
         System.out.print("\nPeriodical payment, using bi-section search: ");
         System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
         System.out.println("number of iterations: " + iterationCounter);
     }
 
-    // End balance calculation using correct formula per period
-    private static double endBalance(double loan, double rate, int n, double payment) {
+    // Computes the ending balance of a loan after all periods
+    private static double endBalance(double loan, double rate, int n, double payment) {    
+        double r = rate / 100.0;          
         double balance = loan;
-        double r = rate / 100.0;
-        for (int i = 0; i < n; i++) {
-            balance = balance * (1 + r) - payment;
+        for(int i = 0; i < n; i++) {
+            balance = (balance - payment) * (1 + r);
         }
         return balance;
     }
 
-    // Brute force solver
+    // Brute force search 
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
         iterationCounter = 0;
-        double payment = loan / n;
-        while (endBalance(loan, rate, n, payment) > epsilon) {
+        double r = rate / 100.0;
+        double payment = loan / n * (1 + r * n / 2.0);  
+        while(endBalance(loan, rate, n, payment) > 0) {
             payment += epsilon;
             iterationCounter++;
         }
         return payment;
     }
 
-    // Bisection solver
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+    // Bisection search
+    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
         iterationCounter = 0;
-        double r = rate / 100.0;
-        double L = loan / n;
-        double H = loan * Math.pow(1 + r, n);
-        double payment = 0;
+        double lo = loan / n;                 
+        double hi = loan * (1 + rate / 100);  
+        double g = 0;
 
-        while ((H - L) > epsilon) {
+        while((hi - lo) > epsilon) {
             iterationCounter++;
-            payment = (L + H) / 2;
-            double f = endBalance(loan, rate, n, payment);
-            if (f > 0) {
-                L = payment;
+            g = (lo + hi) / 2.0;
+            double currentBalance = endBalance(loan, rate, n, g);
+            if(currentBalance > 0) {
+                lo = g;
             } else {
-                H = payment;
+                hi = g;
             }
         }
-
-        return (L + H) / 2;
+        return (lo + hi) / 2.0;
     }
 }
